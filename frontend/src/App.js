c345ae9b9f481688713ce6a9dfb090ba143f028f
Dilukshan <sathiyamurthi.20210851@iit.ac.lk>
2025-05-23 18:05:05 +0530

    UI done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import * as React from 'react';
+import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
+
+import { TaskList, TaskEdit, TaskCreate } from './Tasks';
 
 const dataProvider = simpleRestProvider('http://localhost:8080/api');
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="tasks" list={ListGuesser} />
+      <Resource 
+        name="tasks"
+        list={TaskList}
+        edit={TaskEdit}
+        create={TaskCreate}
+      />
     </Admin>
   );
 }
--- frontend/src/Tasks.js
+++ frontend/src/Tasks.js
@@ -1,30 +1,46 @@
-import * as React from "react";
-import { List, Datagrid, TextField, EditButton, DeleteButton, Edit, SimpleForm, TextInput, Create } from 'react-admin';
+// frontend/src/tasks.js
+import * as React from 'react';
+import {
+  List,
+  Datagrid,
+  TextField,
+  BooleanField,
+  EditButton,
+  DeleteButton,
+  Edit,
+  SimpleForm,
+  TextInput,
+  BooleanInput,
+  Create,
+} from 'react-admin';
 
 export const TaskList = props => (
-    <List {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <TextField source="title" />
-            <EditButton />
-            <DeleteButton />
-        </Datagrid>
-    </List>
+  <List {...props} title="Tasks">
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="title" />
+      <BooleanField source="done" />
+      <EditButton />
+      <DeleteButton />
+    </Datagrid>
+  </List>
 );
 
 export const TaskEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput source="id" disabled />
-            <TextInput source="title" />
-        </SimpleForm>
-    </Edit>
+  <Edit {...props} title="Edit Task">
+    <SimpleForm>
+      <TextInput source="title" />
+      <TextInput source="description" multiline />
+      <BooleanInput source="done" />
+    </SimpleForm>
+  </Edit>
 );
 
 export const TaskCreate = props => (
-    <Create {...props}>
-        <SimpleForm>
-            <TextInput source="title" />
-        </SimpleForm>
-    </Create>
+  <Create {...props} title="New Task">
+    <SimpleForm>
+      <TextInput source="title" />
+      <TextInput source="description" multiline />
+    </SimpleForm>
+  </Create>
 );
