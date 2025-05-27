8312f9e91d733d497d6de934253f64916c991e6e
Dilukshan <sathiyamurthi.20210851@iit.ac.lk>
2025-05-27 16:20:10 +0530

    app updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,39 @@
+// File: src/App.js
 import * as React from 'react';
-import { Admin, Resource } from 'react-admin';
-import dataProvider from './dataProvider';
-import { TaskList, TaskEdit, TaskCreate } from './tasks';
+import { Admin, Resource, defaultTheme, defaultDarkTheme } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import { createTheme } from '@mui/material/styles';
 
-const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="tasks" list={TaskList} edit={TaskEdit} create={TaskCreate} />
-  </Admin>
-);
+import { TaskList, TaskEdit, TaskCreate } from './Tasks';
+
+const dataProvider = simpleRestProvider('/api'); // Relative path, uses proxy
+
+const customTheme = createTheme({
+  palette: {
+    primary: { main: '#1976d2' },
+    secondary: { main: '#dc004e' },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+function App() {
+  return (
+    <Admin
+      dataProvider={dataProvider}
+      theme={customTheme} // Light theme
+      darkTheme={defaultDarkTheme} // Dark theme
+      defaultTheme="light" // Sets initial mode
+    >
+      <Resource 
+        name="tasks" 
+        list={TaskList} 
+        edit={TaskEdit} 
+        create={TaskCreate} 
+      />
+    </Admin>
+  );
+}
 
 export default App;
